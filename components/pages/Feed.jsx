7921508f4eff39a8e00cfba27519311e08762c20
Admin <support@unlimitpotential.com>
2023-01-26 23:11:47 -0500

    a

--- components/pages/Feed.jsx
+++ components/pages/Feed.jsx
@@ -78,28 +78,28 @@ const Feed = () => {
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.32.05_AM_i7dtzd.png" />
             </IonThumbnail>
-            <a href='https://studio.unlimitpotential.com/'> <IonLabel>Launch</IonLabel></a>
+            <a href='https://studio.unlimitpotential.com/'> <IonLabel>Launch App</IonLabel></a>
           </IonItem>
 
           <IonItem>
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.32.20_AM_zltc7j.png" />
             </IonThumbnail>
-            <a href='https://studio.unlimitpotential.com/'>  <IonLabel>Launch</IonLabel></a>
+            <a href='https://studio.unlimitpotential.com/'>  <IonLabel>Launch App</IonLabel></a>
           </IonItem>
 
           <IonItem>
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.31.57_AM_gnw5wi.png" />
             </IonThumbnail>
-            <a href='https://studio.unlimitpotential.com/'>  <IonLabel>Launch</IonLabel></a>
+            <a href='https://studio.unlimitpotential.com/'>  <IonLabel>Launch App</IonLabel></a>
           </IonItem>
 
           <IonItem lines="none">
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.32.11_AM_f7flvc.png" />
             </IonThumbnail>
-            <a href='https://studio.unlimitpotential.com/'>  <IonLabel>Launch</IonLabel></a>
+            <a href='https://studio.unlimitpotential.com/'>  <IonLabel>Launch App</IonLabel></a>
           </IonItem>
         </IonList>
       </IonCardContent>
