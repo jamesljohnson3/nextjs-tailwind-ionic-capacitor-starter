fa3068a107c2f092df9f81a38e215cb0e0048784
Admin <support@unlimitpotential.com>
2023-01-26 23:40:51 -0500

    b

--- components/pages/Feed.jsx
+++ components/pages/Feed.jsx
@@ -77,28 +77,28 @@ const Feed = () => {
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.32.05_AM_i7dtzd.png" />
             </IonThumbnail>
-            <a href='https://studio.unlimitpotential.com/'> <IonLabel>Launch App</IonLabel></a>
+            <a href='https://studio.unlimitpotential.com/'> <IonLabel>Switch Mod</IonLabel></a>
           </IonItem>
 
           <IonItem>
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.32.20_AM_zltc7j.png" />
             </IonThumbnail>
-            <a href='https://connect.unlimitpotential.com/'>  <IonLabel>Launch App</IonLabel></a>
+            <a href='https://connect.unlimitpotential.com/'>  <IonLabel>Recent Posts</IonLabel></a>
           </IonItem>
 
           <IonItem>
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.31.57_AM_gnw5wi.png" />
             </IonThumbnail>
-            <a href='https://accounts.unlimitpotential.com/me'>  <IonLabel>Launch App</IonLabel></a>
+            <a href='https://accounts.unlimitpotential.com/me'>  <IonLabel>Command Center</IonLabel></a>
           </IonItem>
 
           <IonItem lines="none">
             <IonThumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://res.cloudinary.com/unlimitednow/image/upload/v1673246648/Screen_Shot_2023-01-09_at_1.32.11_AM_f7flvc.png" />
             </IonThumbnail>
-            <a href='https://command.unlimitpotential.com/'>  <IonLabel>Start Challenge</IonLabel></a>
+            <a href='https://command.unlimitpotential.com/quiz/63cd86e62525650012f2ad66'>  <IonLabel>Start Challenge</IonLabel></a>
           </IonItem>
         </IonList>
       </IonCardContent>
