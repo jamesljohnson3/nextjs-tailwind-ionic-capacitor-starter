59de50235dd09ff4a5eb71e1d3efacffaca56973
Admin <support@unlimitpotential.com>
2023-01-26 20:24:46 -0500

    h

--- store/index.js
+++ store/index.js
@@ -1,19 +1,35 @@
 import { Store as PullStateStore } from 'pullstate';
-
-import { lists, homeItems, notifications } from '../mock';
+import axios from 'axios';
 
 const Store = new PullStateStore({
-  safeAreaTop: 0,
-  safeAreaBottom: 0,
-  menuOpen: false,
-  notificationsOpen: false,
-  currentPage: null,
-  homeItems,
-  lists,
-  notifications,
-  settings: {
-    enableNotifications: true,
-  },
+safeAreaTop: 0,
+safeAreaBottom: 0,
+menuOpen: false,
+notificationsOpen: false,
+currentPage: null,
+homeItems: [],
+lists: [],
+notifications: [],
+settings: {
+enableNotifications: true,
+},
 });
+
+const fetchData = async () => {
+try {
+const homeItemsResponse = await axios.get('https://connect.unlimitednow.site/homeItems');
+const listsResponse = await axios.get('https://connect.unlimitednow.site/lists');
+const notificationsResponse = await axios.get('https://connect.unlimitednow.site/notifications');
+Store.update(s => {
+s.homeItems = homeItemsResponse.data;
+s.lists = listsResponse.data;
+s.notifications = notificationsResponse.data;
+});
+} catch (error) {
+console.error(error);
+}
+};
+
+fetchData();
 
 export default Store;
